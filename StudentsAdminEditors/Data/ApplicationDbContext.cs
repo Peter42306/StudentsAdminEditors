6f432693f201de9bbfce96fdbb5338ce8d102efe
Petr Zalizko <pzalizko@gmail.com>
2025-04-15 23:01:54 +0300

    Add scaffolded Students CRUD with views, SQLite

--- StudentsAdminEditors/Data/ApplicationDbContext.cs
+++ StudentsAdminEditors/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using StudentsAdminEditors.Models;
 
 namespace StudentsAdminEditors.Data
 {
@@ -9,5 +10,7 @@ namespace StudentsAdminEditors.Data
             : base(options)
         {
         }
+
+        public DbSet<Student> Students { get; set; }
     }
 }
---
+++ StudentsAdminEditors/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsAdminEditors.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        public string? PhotoPath { get; set; }
+
+    }
+}
